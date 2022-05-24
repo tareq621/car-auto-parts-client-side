132963a6d08119e778e0b6ca962d8f008cfe8735
tareq621 <mirtareq621@gmail.com>
2022-05-24 15:09:07 +0600

    parts section

---
+++ src/Pages/Home/Product.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Product = ({ product }) => {
+    const { name, img, price, availableQuantity, orderQuantity, description } = product;
+    return (
+        <div class="card w-96 bg-base-100 shadow-xl">
+            <figure class="px-10 pt-10">
+                <img src={img} alt="" class="rounded-xl" />
+            </figure>
+            <div class="card-body pt-2">
+                <div className='flex justify-between'>
+                    <h4 className='text-xl font-bold'>{name}</h4>
+                    <h6 className='text-md text-primary font-bold'>Per Price <span className='text-primary'>${price}</span></h6>
+                </div>
+                <div className=''>
+                    <p className='text-sm font-bold'>Min. order: <span className='text-primary'>{orderQuantity}</span> Pieces</p>
+                    <p className='text-sm font-bold pt-1'>In stock now: <span className='text-primary'>{availableQuantity} </span>Pieces</p>
+                </div>
+                <p>{description}</p>
+                <div className='mt-2'>
+                    <button class="btn btn-primary hover:text-white">Purchase</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Product;
--- src/Pages/Home/Products.js
+++ src/Pages/Home/Products.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Product from './Product';
 
 const Products = () => {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
     return (
         <div>
-            
+            <h2 className='text-center text-4xl mt-4'>Latest Products</h2>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-12'>
+                {
+                    products.map(product => <Product
+                        key={product._id}
+                        product={product}
+                    ></Product>)
+                }
+            </div>
         </div>
     );
 };
