03bba0930b960ebab333204159e88daca192d4e6
tareq621 <mirtareq621@gmail.com>
2022-05-30 12:15:57 +0600

    .env.local

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDHHNsDJ4jXlLiimBN9x10rbYXJhBkw9RU",
-    authDomain: "car-auto-parts-1b7b2.firebaseapp.com",
-    projectId: "car-auto-parts-1b7b2",
-    storageBucket: "car-auto-parts-1b7b2.appspot.com",
-    messagingSenderId: "816141020651",
-    appId: "1:816141020651:web:028cd0837d9af71286fdae"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
